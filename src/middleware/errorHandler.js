f023b1e0094a45a35e90bc57aa1dca902b727400
TatiYas <iasinovska_lawyer@ukr.net>
2026-01-18 23:58:29 +0100

    Update errorHandler.js

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,34 +1,20 @@
-// src/server.js
+import { HttpError } from "http-errors";
 
-import express from "express";
-import "dotenv/config";
-import cors from "cors";
-// Імпортуємо middleware
-import { errors } from "celebrate";
-import { connectMongoDB } from "./db/connectMongoDB.js";
-import { logger } from "./middleware/logger.js";
-import { notFoundHandler } from "./middleware/notFoundHandler.js";
-import { errorHandler } from "./middleware/errorHandler.js";
-import studentsRoutes from "./routes/studentsRoutes.js";
+export const errorHandler = (err, req, res, next) => {
+  console.error("Error Middleware:", err);
 
-const app = express();
-const PORT = process.env.PORT ?? 3000;
+  // Http Handler error.
+  if (err instanceof HttpError) {
+    return res.status(err.status).json({
+      message: err.message || err.name,
+    });
+  }
 
-app.use(logger);
-app.use(express.json());
-app.use(cors());
+  const isProd = process.env.NODE_ENV === "production";
 
-app.use(studentsRoutes);
-
-// обробка 404
-app.use(notFoundHandler);
-// обробка помилок від celebrate (валідація)
-app.use(errors());
-// глобальна обробка інших помилок
-app.use(errorHandler);
-
-await connectMongoDB();
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  res.status(500).json({
+    message: isProd
+      ? "Something went wrong. Please try again later."
+      : err.message,
+  });
+};

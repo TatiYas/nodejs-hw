cb273680822d8e247ee8014a5a29321e583557ed
TatiYas <iasinovska_lawyer@ukr.net>
2026-01-18 21:51:15 +0100

    Update note.js

--- src/models/note.js
+++ src/models/note.js
@@ -1,37 +1,28 @@
-// src/models/student.js
-
 import { Schema } from 'mongoose';
 import { model } from 'mongoose';
 
 const noteSchema = new Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-      trim: true, // прибирає пробіли на початку та в кінці
-    },
-    age: {
-      type: Number,
-      required: true,
-    },
-    gender: {
-      type: String,
-      required: true,
-      enum: ['male', 'female', 'other'],
-    },
-    avgMark: {
-      type: Number,
-      required: true,
-    },
-    onDuty: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  {
-    timestamps: true,
-    versionKey: false,
-  },
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true, // Trim spaces.
+    },
+    content: {
+      type: String,
+      default: '',
+      trim: true,
+    },
+    tag: {
+      type: String,
+      default: 'Todo',
+      enum: ['Work', 'Personal', 'Meeting', 'Shopping', 'Ideas', 'Travel', 'Finance', 'Health', 'Important', 'Todo'],
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
 );
 
 export const Note = model('Note', noteSchema);
